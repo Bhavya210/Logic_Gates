ee0017ab586498c14c6f05de0aaa8890d5e20bb5
Bhavya210 <98009129+Bhavya210@users.noreply.github.com>
2023-11-30 20:57:28 +0530

    Update and_gate.vhdl

--- and_gate.vhdl
+++ and_gate.vhdl
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 30.11.2023 19:57:23
--- Design Name: 
--- Module Name: and_gate - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
